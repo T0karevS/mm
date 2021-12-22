3816bcb3c437fe09114d13ccc3e1b0dce65f9c9a
T0karevS <90570998+T0karevS@users.noreply.github.com>
2021-12-22 09:20:38 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -9,51 +9,52 @@
 </head>
 <body>
     <form method="post" class="form1">
-    <input type="text" id="zero" class="zero" name="zero" placeholder="a">
-    <input type="text" id="1st" class="1st" name="1st" placeholder="b">
-    <input type="text" id="2nd" class="2nd" name="2nd" placeholder="c">
-    <input type="text" id="3rd" class="3rd" name="3rd" placeholder="d">
-    <input type="text" id="4th" class="4th" name="4th" placeholder="e">
-    <input type="text" id="5th" class="5th" name="5th" placeholder="f">
-    <input type="text" id="6th" class="6th" name="6th" placeholder="g">
-    <input type="text" id="7th" class="7th" name="7th" placeholder="h">
-    <input type="text" id="8th" class="8th" name="8th" placeholder="i">
-    <input type="text" id="9th" class="9th" name="9th" placeholder="j">
-    <input type="text" id="10th" class="10th" name="10th" placeholder="k">
-    <input type="text" id="11th" class="11th" name="11th" placeholder="l">
-    <button class="btn">Отправить</button>
+
     <div class="div0">
         <div class="div1">
             <p class="p-0">0</p>
+            <input type="text" id="zero" class="zero" name="zero" placeholder="a">
             <img class="arrow1" src="svg/1.svg" >
             <p class="p-1">1</p>
+            <input type="text" id="1st" class="1st" name="1st" placeholder="b">
             <img class="arrow1" src="svg/1.svg" >
             <p class="p-2">2</p>
+            <input type="text" id="2nd" class="2nd" name="2nd" placeholder="c">
             <img class="arrow1" src="svg/1.svg" >
+            <input type="text" id="4th" class="4th" name="4th" placeholder="e" style="margin-top: 40px;">
             <p class="p-4">4</p>
+            <input type="text" id="5th" class="5th" name="5th" placeholder="f">
             <img class="arrow1" src="svg/1.svg" >
             <p class="p-6">6</p>
+            <input type="text" id="6th" class="6th" name="6th" placeholder="g">
             <img class="arrow1" src="svg/1.svg" >
             <p class="p-8">8</p>
+            <input type="text" id="10th" class="10th" name="10th" placeholder="k">
             <img class="arrow1" src="svg/1.svg" >
             <p class="p-9">9</p>
+            <input type="text" id="11th" class="11th" name="11th" placeholder="l">
             <img class="arrow1" src="svg/1.svg" >
             <p class="p-10">10</p>
         </div>
         <div class="div2">
+        <input type="text" id="3rd" class="3rd" name="3rd" placeholder="d" style="margin-right: -40px;">
             <img class="arrow2" src="svg/2.svg"  >     
-                <p class="p-3" style="margin-top: 10px;  margin-left: -30px;">3</p>
-                <img class="arrow1" src="svg/1.svg" style=" height: 56px; width: 130px;" >
+             <p class="p-3" style="margin-top: 10px;  margin-left: -30px;">3</p>
+                <img class="arrow3" src="svg/3106303.svg">
+                <input type="text" id="7th" class="7th" name="7th" placeholder="h" style="margin-left: -90px;">
+                <img class="arrow1" src="svg/1.svg" style=" height: 56px; margin-left: 0px" >
                 <p class="p-7" >7</p>   
-                <img class="arrow3" src="svg/3106303.svg" style="height: 66px; width: 100px; " >         
+                <input type="text" id="9th" class="9th" name="9th" placeholder="j">
+                <img class="arrow3" src="svg/3106303.svg" style="height: 66px; width: 100px; margin-left: -60px; " >         
             </div>
             <div class="div3">
                 <img class="arrow2" src="svg/2.svg" >  
                 <p class="p-5" style="margin-left: -20px;">5</p> 
-                <img class="arrow3" src="svg/3106303.svg" >         
+                <input type="text" id="8th" class="8th" name="8th" placeholder="i">
+                <img class="arrow3" src="svg/3106303.svg" style="margin-left: -70px;" >         
             </div>       
     </div>
-    
+    <button class="btn">Отправить</button>    
     <div class="info"></div>
     </form>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
