9de6b02f9a84868593d190dc8847ddb40e452354
T0karevS <90570998+T0karevS@users.noreply.github.com>
2021-12-24 00:14:00 +0400

    Add files via upload

--- kod.php
+++ kod.php
@@ -7,5 +7,30 @@ $result1 = a("zero") + a("1st") + a("2nd") + a("5th") + a("6th") + a("10th")+ a(
 $result2 = a("zero") + a("1st") + a("3rd") + a("4th") + a("5th") + a("6th") + a("10th")+ a("11th");
 $result3 = a("zero") + a("1st") + a("3rd") + a("7th") + a("9th") + a("10th")+ a("11th");
 $result4 = a("zero") + a("1st") + a("3rd") + a("8th") + a("9th") + a("10th")+ a("11th");
+$R=max($result1, $result2, $result3, $result4);
 echo "Критический путь:";
-echo max($result1, $result2, $result3, $result4);
+echo "<br>";
+echo $R;
+echo "<br>";
+echo "L критический:";
+echo "<br>";
+if( $R==$result1)
+{
+    echo "0-1-2-4-6-8-9-10";
+    echo "<br>";
+}
+elseif( $R==$result2)
+{
+    echo "0-1-2-3-4-6-8-9-10";
+    echo "<br>";
+}
+elseif($R==$result3)
+{
+    echo "0-1-2-3-7-8-9-10";
+    echo "<br>";
+}
+elseif($R==$result4)
+{
+    echo "0-1-2-3-5-7-8-9-10";
+    echo "<br>";
+}
